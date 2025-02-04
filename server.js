8b8cf874530914d3d518ecf00a092cb3b6cf9748
Louiskok <bluecrab_888@hotmail.com>
2025-02-04 11:33:37 +0800

    new codes

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ dotenv.config();
 
 // Create an express app
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('Error connecting to MongoDB:', err));
 
